faf16a736370d8975ecfbf1bad593532bd10d5a2
foresitegroup <mark@foresitegrp.com>
2018-01-09 16:58:52 -0600

    2018-01-09

---
+++ inc/find-part-home.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+  var id_fp = $('#home-find-part');
+  var id_man = $('#home-manufacturer');
+  var id_mod = $('#home-model');
+  var id_ts = $('#home-type-size');
+  var id_fmp = $('#home-find-my-part');
+  var id_fpr = $('#home-find-part-results');
+  var fpr_id = 'home-find-part-results';
+  
+  // Disable fields
+  $(id_mod).closest('.select').addClass("disabled");
+  $(id_ts).closest('.select').addClass("disabled");
+  $(id_mod).add(id_ts).add(id_fmp).prop('disabled', true);
+  $(id_fpr).css('display', 'none');
+
+  var manufacturer;
+
+  $(id_man).change(function() {
+    manufacturer = $(this).val();
+
+    $.ajax({
+      type: "POST", cache: false,
+      url: "ajax-model.php",
+      data: 'manufacturer='+ manufacturer,
+      success: function(html) { $(id_mod).html(html); }
+    });
+    
+    $(id_fpr).css('display', 'none');
+    $(id_mod).add(id_ts).add(id_fmp).prop('disabled', true);
+    $(id_mod).closest('.select').addClass("disabled");
+    $(id_ts).closest('.select').addClass("disabled");
+    $(id_ts).val('');
+
+    if ($(this).val()) {
+      $(id_mod).prop('disabled', false);
+      $(id_mod).closest('.select').removeClass("disabled");
+    }
+  });
+
+  $(id_mod).change(function() {
+    $.ajax({
+      type: "POST", cache: false,
+      url: "ajax-type-size.php",
+      data: 'manufacturer='+ manufacturer +'&model='+ $(this).val(),
+      success: function(html) { $(id_ts).html(html); }
+    });
+    
+    $(id_fpr).css('display', 'none');
+    $(id_ts).add(id_fmp).prop('disabled', true);
+    $(id_ts).closest('.select').addClass("disabled");
+    if ($(this).val()) {
+      $(id_ts).prop('disabled', false);
+      $(id_ts).closest('.select').removeClass("disabled");
+    }
+  });
+
+  $(id_ts).change(function() {
+    $(id_fpr).css('display', 'none').removeClass('part-found');
+    $(id_fmp).prop('disabled', true);
+    if ($(this).val()) $(id_fmp).prop('disabled', false);
+  });
+
+  $(id_fp).submit(function(event) {
+    event.preventDefault();
+
+    $.ajax({
+      type: 'POST',
+      url: $(id_fp).attr('action'),
+      data: $(id_fp).serialize() + '&fpr_id='+fpr_id+'&src=ajax'
+    })
+    .done(function(response) {
+      $(id_fpr).css('display', 'block').html(response);
+    });
+  });
+});
--- index.php
+++ index.php
@@ -120,12 +120,58 @@ $PageClass = "home";
     </div>
 
     <div id="waypoint-trigger"></div>
+    
+    <div id="home-lookup">
+      <div class="site-width">
+        <h2>Elastomeric Product Lookup</h2>
 
-    <div class="site-width">
-      CONTENT HERE
+        <script type="text/javascript" src="inc/find-part-home.js"></script>
+
+        <form action="ajax-find-part.php" method="POST" id="home-find-part" class="lookup-form">
+          <div class="select-man">
+            <label for="home-manufacturer">Manufacturer</label>
+            <div class="select">
+              <select name="manufacturer" id="home-manufacturer">
+                <option value="">Select...</option>
+                <?php
+                $manresult = $mysqli->query("SELECT * FROM products GROUP BY competitor ORDER BY competitor ASC");
+
+                while($manrow = $manresult->fetch_array(MYSQLI_ASSOC)) {
+                  echo '<option value="' . $manrow['competitor'] . '">' . $manrow['competitor'] . '</option>';
+                }
+
+                $manresult->close();
+                ?>
+              </select>
+            </div>
+          </div>
+          
+          <div class="select-mod">
+            <label for="home-model">Model</label>
+            <div class="select">
+              <select name="model" id="home-model">
+                <option value="">Select...</option>
+              </select>
+            </div>
+          </div>
+          
+          <div class="select-ts">
+            <label for="home-type-size">Type/Size</label>
+            <div class="select">
+              <select name="type-size" id="home-type-size">
+                <option value="">Select...</option>
+              </select>
+            </div>
+          </div>
+
+          <button id="home-find-my-part" class="find-my-part">Find My Part</button>
+        </form>
+      </div>
+
+      <div id="home-find-part-results" class="find-part-results"></div>
     </div>
 
-    <div style="width: 500px; height: 2000px; margin: 0 auto 2em; background-image: linear-gradient(to bottom, red, orange, yellow, green, blue, indigo, violet);"></div>
+    <div style="width: 500px; height: 2000px; margin: 0 auto 2em; background-image: linear-gradient(to bottom, red, orange, yellow, green, blue, indigo, violet);">HELLO WORLD</div>
     
   </body>
 </html>
