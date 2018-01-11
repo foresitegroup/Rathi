f6163afe3d89cde90522cc449313967b8f9d8152
foresitegroup <mark@foresitegrp.com>
2018-01-11 16:59:40 -0600

    2018-01-11

--- index.php
+++ index.php
@@ -186,13 +186,103 @@ $PageClass = "home";
       <div id="home-find-part-results" class="find-part-results"></div>
     </div>
 
+    <script type="text/javascript" src="inc/jquery.cycle2.min.js"></script>
+    <script type="text/javascript" src="inc/jquery.cycle2.carousel.min.js"></script>
+
+    <script type="text/javascript">
+      $(document).ready(function() {
+        function buildCarousel() {
+          var slides = 4;
+          var startslide = 5;
+
+          if (window.innerWidth < 901) { slides = 2; startslide = 0; }
+          if (window.innerWidth < 481) { slides = 1; startslide = 0; }
+
+          $('#home-blog').cycle({
+            timeout: 0,
+            slides: "> a",
+            prev: "#prev",
+            next: "#next",
+            fx: 'carousel',
+            carouselFluid: 'true',
+            carouselVisible: slides,
+            startingSlide: startslide
+          });
+        }
+
+        function resizeCarousel() {
+          $('#home-blog').cycle('destroy');
+          buildCarousel();
+        }
+
+        buildCarousel();
+        
+        $(window).resize(function(){
+          setTimeout(function() { resizeCarousel(); },100);
+        });
+      });
+    </script>
+
     <div id="home-blog">
-      BLOG STUFF HERE
+      <a href="#">
+        <div style="background-image: url(https://fillmurray.com/400/400);">
+          <div class="title">Blog Title One</div>
+          <div class="author">By Dougie Jones</div>
+        </div>
+      </a>
+      <a href="#">
+        <div style="background-image: url(https://fillmurray.com/410/410);">
+          <div class="title">Blog Title Two Is Kind of Long</div>
+          <div class="author">By Chip Dipson</div>
+        </div>
+      </a>
+      <a href="#">
+        <div style="background-image: url(https://fillmurray.com/420/420);">
+          <div class="title">Blog Title Three Is Really Quite Long</div>
+          <div class="author">By Dip Dobson</div>
+        </div>
+      </a>
+      <a href="#">
+        <div style="background-image: url(https://fillmurray.com/430/430);">
+          <div class="title">Blog Title Four</div>
+          <div class="author">By Dougie Jones</div>
+        </div>
+      </a>
+      <a href="#">
+        <div style="background-image: url(https://fillmurray.com/440/440);">
+          <div class="title">Blog Title Five</div>
+          <div class="author">By Dougie Jones</div>
+        </div>
+      </a>
+      <a href="#">
+        <div style="background-image: url(https://fillmurray.com/450/450);">
+          <div class="title">Blog Title Six</div>
+          <div class="author">By Dougie Jones</div>
+        </div>
+      </a>
+
+      <span href="#" id="prev"><i class="fa fa-angle-left" aria-hidden="true"></i></span>
+      <span href="#" id="next"><i class="fa fa-angle-right" aria-hidden="true"></i></span>
     </div>
 
     <footer>
       <div class="site-width">
-        FOOTER
+        <div class="logo"><img src="images/logo.png" alt="Rathi North America"></div>
+
+        <div class="address">
+          Rathi North America<br>
+          N56W13855 Silver Spring Dr.<br>
+          Menomonee Falls, WI 53051
+        </div>
+
+        <div class="social">
+          <a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+          <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+          <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+          <br>
+
+          <a href="tel:12816900741">1.281.690.0741</a>
+        </div>
       </div>
     </footer>
 
