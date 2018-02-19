4056d9c4e0c1302eb11acd0841abc7117c8a494a
foresitegroup <mark@foresitegrp.com>
2018-02-19 11:34:01 -0600

    Add analytics

--- header.php
+++ header.php
@@ -42,6 +42,15 @@ function email($address, $name="") {
         });
       });
     </script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-114304978-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-114304978-1');
+    </script>
   </head>
   <body<?php if (isset($PageClass)) echo ' class="' . $PageClass . '"'; ?>>
 
