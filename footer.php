93a61e66b3ff5cfdafd757f433eb58e8359b358a
marklippert <lippert@gmail.com>
2020-06-01 14:35:01 -0500

    Remove US contact info

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
         <div class="logo"><img src="<?php echo $TopDir; ?>images/logo.png" alt="Rathi North America"></div>
 
         <div class="address">
-          Rathi North America<br>
+          <!-- Rathi North America<br>
           N56W13855 Silver Spring Dr.<br>
-          Menomonee Falls, WI 53051
+          Menomonee Falls, WI 53051 -->
         </div>
 
         <div class="social">
@@ -15,7 +15,7 @@
           <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
           <br>
 
-          <a href="tel:3174905542">317.490.5542</a>
+          <a href="tel:912067661201">91.20.67661.201</a>
         </div>
       </div>
     </footer>
